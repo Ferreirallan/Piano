6b6d1bbe9ed361b488f4af87625a62882f4b8290
Allan Ferreira <al.portoferreira@gmail.com>
2025-04-23 17:47:42 -0300

    audio

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-const pianoKeys = document.querySelectorAll(".piano-keys")
+const pianoKeys = document.querySelectorAll(".piano-keys .key");
+
+let audio = new Audio("tunes/a.wav"); //por padrão, a origem do áudio é "a" melodia
+
+const playTune = (key) => {
+
+}
+
+pianoKeys.forEach(key => {
+    // chamando a função playTune passando o valor da chave de dados como argumento
+    key.addEventListener("click", () => plauTune(key.dataset.key))
+
+});
